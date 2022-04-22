99ef15769d1ee82f528dc7bde23f3f075d81c877
LaupWing <loc_strict@hotmail.com>
2022-04-22 17:27:24 +0200

    test for nft

--- test/sample-test.js
+++ test/sample-test.js
@@ -20,5 +20,25 @@ describe("NFTMarket", function () {
 
       await nft.createToken('https://www.mytokenlocation.com')
       await nft.createToken('https://www.mytokenlocation2.com')
+
+      await market.createMarketItem(nftContractAddress, 1, auctionPrice, {value:listingPrice})
+      await market.createMarketItem(nftContractAddress, 2, auctionPrice, {value:listingPrice})
+
+      const [_, buyerAddress] = await ethers.getSigners()
+
+      await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, {value: auctionPrice})
+
+      let items = await market.fetchMarketItems()
+      items = await Promise.all(items.map(async i =>{
+         const tokenUri = await nft.tokenURI(i.tokenId)
+         return {
+            price: i.price.toString(),
+            tokenId: i.tokenId.toString(),
+            seller: i.seller,
+            owner: i.owner,
+            tokenUri
+         }
+      }))
+      console.log(items)
    })
 })
