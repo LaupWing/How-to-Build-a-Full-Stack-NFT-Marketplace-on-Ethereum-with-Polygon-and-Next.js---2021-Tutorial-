0f560dd5f0214f05ab8ca0768fc787915097e377
LaupWing <loc_strict@hotmail.com>
2022-04-23 14:55:08 +0200

    delpoy

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+   const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+   const nftMarket = await NFTMarket.deploy();
+
+   await nftMarket.deployed();
+
+   console.log("NFTMarket deployed to:", nftMarket.address);
+}
+
+main()
+   .then(() => process.exit(0))
+   .catch((error) => {
+      console.error(error);
+      process.exit(1);
+   });
