9c2dde9fab9304f98e0880cb399b8b09e85767c7
LaupWing <loc_strict@hotmail.com>
2022-04-23 14:55:55 +0200

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,10 +3,13 @@ const hre = require("hardhat");
 async function main() {
    const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
    const nftMarket = await NFTMarket.deploy();
-
    await nftMarket.deployed();
-
    console.log("NFTMarket deployed to:", nftMarket.address);
+
+   const NFT = await hre.ethers.getContractFactory("NFT");
+   const nft = await NFT.deploy(nftMarket.address);
+   await nft.deployed();
+   console.log("NFT deployed to:", nft.address);
 }
 
 main()
